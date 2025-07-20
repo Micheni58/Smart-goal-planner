39bf72f2991117bd85d517a6d5ed323ec53f1eec
Micheni58 <michenicaleb@gmail.com>
2025-07-20 17:08:22 +0300

    Add grid styling to cards

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,12 @@ function App() {
     setGoals([...goals, newGoal]); 
   }
   function handleDelete(goalId){
-    fetch(`http://localhost:3000/goals/${goalId}`,{
+    fetchfetch(`http://localhost:3000/goals/${goalId}`,{
       method: "DELETE",
     })
     .then((res) =>{
       if(res.ok) {
-        setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== goalId))
+        setGoals((prevGoals) => prevGoals.filter((goal) => goal.id != goalId));
       }
     })
   }
@@ -32,7 +32,10 @@ function App() {
     <>
       <h1>App Component</h1>
       <GoalForm onAddGoal={handleAddGoal} />
-      {goalList}
+      <div className='cards'>
+         {goalList}
+      </div>
+     
     </>
   );
 }
