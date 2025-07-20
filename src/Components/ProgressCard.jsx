18c94dd147e8ff6d1649d392637ca0aea45c822b
Micheni58 <michenicaleb@gmail.com>
2025-07-20 17:57:54 +0300

    Add progress card

---
+++ src/Components/ProgressCard.jsx
@@ -0,0 +1,10 @@
+function ProgressCard(){
+    return(
+        <>
+        <div className="progress-card">
+            <h1>View Progress</h1>
+        </div>
+        </>
+    )
+}
+export default ProgressCard;
